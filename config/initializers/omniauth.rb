3feb3cfd02b2f7d728b6f24f6a496a6a186da5ba
southwolf <wp.southwolf@gmail.com>
2017-06-03 20:45:00 +0800

    oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,5 @@
-use OmniAuth::Builder do
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :wechat, 'wx36fee8a5da4ac490', '1c696a70389e96785c454aa391d4d1ac'
+  provider :weibo, '4178601071', '7b1cf76af84be22b54973e28f4ede4b3'
   provider :qq_connect, ENV['QQ_CONNECT_APP_KEY'], ENV['QQ_CONNECT_APP_SECRET']
 end
