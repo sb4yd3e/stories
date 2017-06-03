e4ed6574dee5d16a642d28d0e74ead5753ec845b
Your Name <you@example.com>
2017-06-03 20:18:27 +0800

    deploy

--- config/puma.rb
+++ config/puma.rb
@@ -1,12 +1,19 @@
 workers Integer(ENV['WEB_CONCURRENCY'] || 2)
 threads_count = Integer(ENV['MAX_THREADS'] || 5)
 threads threads_count, threads_count
+app_dir = File.expand_path("../..", __FILE__)
+shared_dir = "#{app_dir}/shared"
 
 preload_app!
 
 rackup      DefaultRackup
-port        ENV['PORT']     || 3000
+port        ENV['PORT']     || 80
 environment ENV['RACK_ENV'] || 'development'
+bind "unix://#{shared_dir}/sockets/puma.sock"
+stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+pidfile "#{shared_dir}/pids/puma.pid"
+state_path "#{shared_dir}/pids/puma.state"
+activate_control_app
 
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
