c41170c01e09df7202a8be9adcb727aac0aeae02
southwolf <wp.southwolf@gmail.com>
2017-06-14 11:26:41 +0800

    port

--- config/puma.rb
+++ config/puma.rb
@@ -9,7 +9,7 @@ preload_app!
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'production'
-# bind 'tcp://0.0.0.0:3000'
+bind 'tcp://0.0.0.0:3000'
 stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 pidfile "#{shared_dir}/pids/puma.pid"
 state_path "#{shared_dir}/pids/puma.state"
