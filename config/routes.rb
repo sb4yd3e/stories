f7f2cf2ae469ad9ccfbfbf2be6c878c8deb2d194
southwolf <wp.southwolf@gmail.com>
2017-06-19 11:30:39 +0800

    conf

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
+  resource :wechat, only: [:show, :create]
   root "dashboards#show"
   devise_for :admins, controllers: { sessions: 'admin/sessions' }
   devise_for :users, controllers: { sessions: 'users/sessions', :omniauth_callbacks => "users/omniauth_callbacks" }
